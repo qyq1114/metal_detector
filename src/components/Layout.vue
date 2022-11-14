2a5dd00f4ca20e7340a3b02461804ca5121083cf
Yuqi_qin <qyq1114@qq.com>
2022-11-14 22:06:05 +0800

    footer

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -22,6 +22,23 @@
         <el-main>
             <router-view />
         </el-main>
+        <el-footer height="220px">
+            <div class="footer-content">
+                <div class="logo">
+                    <img src='../assets/images/logo.svg' />
+                    <span>金属表面缺陷检测系统</span>
+                </div>
+                <ul class="content-nav">
+                    <li>
+                        <p>联系方式</p>
+                        <span>邮箱：xxx@163.com</span>
+                        <span>电话：</span>
+                        <span>地址：安徽省宿州市博望路58号</span>
+                    </li>
+                </ul>
+            </div>
+        </el-footer>
+
     </el-container>
 </template>
   
@@ -30,7 +47,7 @@ export default {
     data() {
         return {
             router: true,
-            defaultActive: this.$route.name==='home'?'/home':'/list',
+            defaultActive: this.$route.name === 'home' ? '/home' : '/list',
             isShow: false
         };
     },
@@ -65,27 +82,6 @@ export default {
         align-content: center;
         align-items: center;
 
-        .logo {
-            display: flex;
-            align-items: center;
-            margin-right: 200px;
-
-            img {
-                width: 40px;
-                height: 40px;
-                line-height: 60px;
-                margin-right: 10px;
-            }
-
-            span {
-                color: #215EF3;
-                font-family: 'Arial';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 18px;
-            }
-        }
-
         .el-menu.el-menu--horizontal {
             border: 0;
         }
@@ -106,8 +102,30 @@ export default {
 }
 
 .el-container {
+    .logo {
+        display: flex;
+        align-items: center;
+        margin-right: 200px;
+
+        img {
+            width: 40px;
+            height: 40px;
+            line-height: 60px;
+            margin-right: 10px;
+        }
+
+        span {
+            color: #215EF3;
+            font-family: 'Arial';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+        }
+    }
+
     .el-main {
         padding: 5px 0 0 0;
+        margin-bottom: 40px;
 
         .widFix {
             padding-top: 40px;
@@ -115,6 +133,60 @@ export default {
             margin: 0 auto;
         }
     }
+
+    .el-footer {
+        width: 100%;
+        padding: 0;
+        overflow: hidden;
+        background-color: #ecf1fe;
+        margin-bottom: 0 !important;
+        padding-top: 30px;
+        .logo {
+            img {
+                width: 30px;
+                height: 30px;
+            }
+        }
+        .footer-content {
+            width: 1344px;
+            margin: 0 auto;
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+    
+            .content-nav {
+                display: flex;
+                justify-content: space-around;
+    
+                li {
+                    display: flex;
+                    flex-direction: column;
+                    padding: 0 20px;
+                    //justify-content: center;
+                    align-items: flex-start;
+    
+                    p {
+                        color: #215ef3;
+                        font-family: "PingFang SC";
+                        font-style: normal;
+                        font-weight: 600;
+                        font-size: 16px;
+                        line-height: 22px;
+                        margin-bottom: 20px;
+                    }
+    
+                    span {
+                        font-weight: 400;
+                        font-size: 14px;
+                        line-height: 20px;
+                        color: #3e3e3e;
+                        margin-bottom: 15px;
+                    }
+                }
+            }
+        }
+    }
+    
 }
 </style>
   
